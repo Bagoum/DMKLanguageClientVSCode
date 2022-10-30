dfc2efadcd5aea3427abb93f6d876f0b9930aedc
Bagoum <reneedebatz@gmail.com>
2022-10-30 14:27:42 -0700

    v1.0.0: Support for custom DLLs and inlay hints

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # DMK Scripting Language Extension
 
-This is a language extension for [Danmokou](https://github.com/Bagoum/danmokou)'s scripting language.
+This is a language extension for [Danmokou](https://github.com/Bagoum/danmokou)'s scripting language. It operates over `.bdsl` files.
 
 This extension provides the following features:
 
@@ -9,7 +9,8 @@ This extension provides the following features:
 - Method signature helpers
 - Method documentation on hover
 - Live error detection
-- Warnings 
+- Warnings
+- Inlay hints
 
 
 
@@ -97,8 +98,27 @@ Some syntax highlighting must be configured in settings.json. You can use the fo
 }
 ```
 
+## Extension Settings
 
+There are three options you can find under "DMK Language Server" in the extensions settings. After changing any of these options, you will need to restart the language server. You can do this by pressing ctrl+shift+P to bring up the command palette, then pressing "Restart DMK language server".
+
+- **DLL Path**: If you add new reflectable functions to your repository, by default they will not be visible to the extension, and the extension will give you errors saying that the function could not be found. To resolve this, you can build your Unity project (eg. to a folder `C:\MyUnityProjects\Danmokou\Builds\dllsrc`), and then set the value of this setting to the path of the built DLLs, eg. `C:\MyUnityProjects\Danmokou\Builds\dllsrc\Danmokou_Data\Managed`.
+- **YAML Path**: If you change documentation for any reflectable functions in your repository, including if you add new reflectable functions, by default the documentation will not be visible to the extension **even if you set the DLL path.** This is because the documentation is sourced from docfx yaml files. To update custom documentation, run `docfx` in the `docs` directory of your DMK project, and then set the value of this setting to eg. `C:\MyUnityProjects\Danmokou\docs\api`.
+
+- **Inlay Hints**: set this to true to enable inlay hints for function calls. Note that you also have to enable editor-wide inlay hints as Editor › Inlay Hints: Enabled. Note that you may also want to add VSCode settings configurations for inlay hint styling, such as the following:
+
+  ```
+  "editor.inlayHints.fontSize": 12,
+  "editor.inlayHints.padding": true,
+  "workbench.colorCustomizations": {
+  	"editorInlayHint.background": "#2a2a2a",
+  	"editorInlayHint.foreground": "#bbb"
+  },
+  ```
+
+## Source
 
  The source code for this project is on Github at [Bagoum/DMKLanguageServerVSCode](https://github.com/Bagoum/DMKLanguageClientVSCode). 
 
 This project can be packaged by running `vsce package`, and then installed as a VSCode extension.
+
--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,29 @@ import * as vscode from 'vscode';
 import * as path from "path";
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions, TransportKind, InitializeParams } from 'vscode-languageclient/node';
 import { Trace } from 'vscode-jsonrpc';
+import { start } from 'repl';
 
 let client: LanguageClient;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
-	let serverExe = context.asAbsolutePath("out\\dll\\DMKLanguageServer.exe");
-	let serverOptions: ServerOptions = {
-        run: { command: serverExe, args: [], options: { cwd: path.dirname(serverExe) } },
-        debug: { command: serverExe, args: ["--debug"], options: { cwd: path.dirname(serverExe) } }
-    };
-
-	console.log(process.cwd());
-	console.log(serverExe);
-	console.log(`DMK Language Extension activated`);
+	async function startClient() {
+		let serverExe = context.asAbsolutePath("out\\dll\\DMKLanguageServer.exe");
+		let settings = vscode.workspace.getConfiguration('danmokou');
+		let runOptions = ["--dllpath", settings['DLL Path'], "--ymlpath", settings['YAML Path']];
+		if (settings['Inlay Hints']) {
+			runOptions.push("--inlayHints");
+		}
+		let serverOptions: ServerOptions = {
+			run: { command: serverExe, args: runOptions, options: { cwd: path.dirname(serverExe) } },
+			debug: { command: serverExe, args: ["--debug"].concat(runOptions), options: { cwd: path.dirname(serverExe) } }
+		};
+	
+		console.log(process.cwd());
+		console.log(serverExe);
+		console.log(runOptions);
+		console.log(`DMK Language Extension activated`);
 		let clientOptions: LanguageClientOptions = {
 			documentSelector: [{pattern: '**/*.bdsl'}],
 			synchronize: {
@@ -24,15 +32,22 @@ export async function activate(context: vscode.ExtensionContext) {
 				fileEvents: vscode.workspace.createFileSystemWatcher('**/*.bdsl')
 			},
 		};
+		
+		client = new LanguageClient("DMKLanguageServer", "DMK Language Server", serverOptions, clientOptions);
+		client.setTrace(Trace.Verbose);
+		await client.start();
+	}
+	await startClient();
 	
-	context.subscriptions.push(vscode.commands.registerCommand("bdsl.activate", () => {
-        vscode.window.showInformationMessage("Starting the DMK language server...");
+	context.subscriptions.push(vscode.commands.registerCommand("bdsl.restart", async () => {
+		if (!client) {
+			vscode.window.showErrorMessage("The DMK language server has not started yet.");
+		} else {
+			vscode.window.showInformationMessage("Restarting the DMK language server...");
+			await deactivate();
+			await startClient();
+		}
     }));
-
-	
-	const client = new LanguageClient("DMKLanguageServer", "DMK Language Server", serverOptions, clientOptions);
-    client.setTrace(Trace.Verbose);
-	await client.start();
 }
 
 // this method is called when your extension is deactivated
